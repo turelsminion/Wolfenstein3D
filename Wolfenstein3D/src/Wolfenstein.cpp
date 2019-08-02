b256e1579f88888ce0ecb0455c5730f2df6cd6b6
PaulCastellano <eugengaraz01@gmail.com>
2019-08-02 16:45:52 +0300

    shader + uniform + errors

--- Wolfenstein3D/src/Wolfenstein.cpp
+++ Wolfenstein3D/src/Wolfenstein.cpp
@@ -110,6 +110,8 @@ int main(void) {
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	glfwSwapInterval(1);
+
 	/*Init the glew after the context */
 	if (glewInit() != GLEW_OK) {
 		std::cout << "ERROR" << std::endl;
@@ -147,15 +149,31 @@ int main(void) {
 	std::cout << source.FragmentSource << std::endl;
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 
-	glUseProgram(shader);
+	GLCall(glUseProgram(shader));
 
+	int location = glGetUniformLocation(shader, "u_Color");
+	ASSERT(location != -1);
+	GLCall(glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f));
+
+
+	float r = 0.0f;
+	float increment = 0.05f;
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
+
+		GLCall(glUniform4f(location, r, 0.3f, 0.8f, 1.0f));
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+
+		if (r > 1.0f)
+			increment = -0.05f;
+		else if (r < 0.0f)
+			increment = 0.05f;
+
+		r += increment;
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
